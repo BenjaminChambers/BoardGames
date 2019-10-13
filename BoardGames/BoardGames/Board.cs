9388a4a74355d65e0e8eff438367086db068edad
Benjamin Chambers <benjamindchambers@outlook.com>
2019-10-13 07:29:03 -0600

    Initial stab at the generic Board

---
+++ BoardGames/BoardGames/Board.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace BoardGames
+{
+    public class Board<T>
+    {
+        public Board(int Width, int Height)
+        {
+            this.Width = Width;
+            this.Height = Height;
+            var a = new T[Width * Height];
+            Cells = new ReadOnlyCollection<T>(a);
+        }
+        public Board(int Width, int Height, IList<T> Source)
+        {
+            this.Width = Width;
+            this.Height = Height;
+            Cells = new ReadOnlyCollection<T>(Source);
+        }
+
+        public readonly int Width;
+        public readonly int Height;
+        public ReadOnlyCollection<T> Cells;
+    }
+}
