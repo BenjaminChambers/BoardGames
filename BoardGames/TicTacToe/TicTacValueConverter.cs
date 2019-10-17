6beb662ca75c8a1c176dc8167c0ab4b10b3108a6
Benjamin Chambers <benjamindchambers@outlook.com>
2019-10-17 08:35:27 -0600

    Added a basic valueconverter for TicTacMove

--- BoardGames/TicTacToe/TicTacValueConverter.cs
+++ BoardGames/TicTacToe/TicTacValueConverter.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
+using System.Windows.Data;
 
 namespace TicTacToe
 {
-    class TicTacValueConverter
+    class TicTacValueConverter : IValueConverter
     {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => (BoardGames.TicTacToe.TicTacMove)value switch {
+            BoardGames.TicTacToe.TicTacMove.X => "X",
+            BoardGames.TicTacToe.TicTacMove.O => "O",
+            _ => string.Empty
+            };
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => null;
     }
 }
