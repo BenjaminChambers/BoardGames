2d7094a83dfcc5ad05415b2343aab3fdb113adf4
Benjamin Chambers <benjamindchambers@outlook.com>
2019-10-22 12:27:01 -0600

    Return result of Run methods

---
+++ BoardGames/BoardGames/Run.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoardGames
+{
+    public struct Run<T>
+    {
+        public int StartRow;
+        public int StartColumn;
+        public int EndRow;
+        public int EndColumn;
+        public T Value;
+    }
+}
