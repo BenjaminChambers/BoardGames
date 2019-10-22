4e92bf790a3652f85f5e0ff1c72ed8b829855b5a
Benjamin Chambers <benjamindchambers@outlook.com>
2019-10-22 12:25:46 -0600

    Stub for Gomoku/Stone classes

---
+++ BoardGames/BoardGames/Gomoku/Gomoku.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoardGames.Gomoku
+{
+    public class Gomoku : Board<Stone>
+    {
+        public Gomoku()
+            : base(19, 19) => Turn = Stone.Black;
+        public Gomoku(Gomoku Source)
+            : base(Source) => Turn = Source.Turn;
+        public Gomoku(IEnumerable<Stone> Source, Stone Turn)
+            : base(3, 3, Source) => this.Turn = Turn;
+
+
+        private Gomoku(Gomoku Source, IEnumerable<(int Index, Stone Value)> Changes, Stone Turn)
+            : base(Source, Changes) => this.Turn = Turn;
+        private Gomoku(Gomoku Source, bool FlipHorizontal, bool FlipVertical)
+            : base(Source, FlipHorizontal, FlipVertical) => Turn = Source.Turn;
+        private Gomoku(Gomoku Source, int ClockwiseRotations)
+            : base(Source, ClockwiseRotations) => Turn = Source.Turn;
+
+
+        public Gomoku Rotate(int Clockwise)
+            => new Gomoku(this, Clockwise);
+        public Gomoku Flip(bool Horizontal, bool Vertical)
+            => new Gomoku(this, Horizontal, Vertical);
+
+
+        public Gomoku Play(int Index)
+        {
+            if (All[Index] != Stone.Empty)
+                throw new InvalidOperationException("Cannot place stone in non-empty cell.");
+
+            return new Gomoku(this, new[] { (Index, Turn) }, (Turn == Stone.Black) ? Stone.White : Stone.Black);
+        }
+        public Gomoku Play(int Column, int Row)
+            => Play(Row * 19 + Column);
+
+
+        public readonly Stone Turn;
+    }
+}
