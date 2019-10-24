cdc11bbcb25dfa5f9ec1a3594df1cbe27bd82fcc
Benjamin Chambers <benjamindchambers@outlook.com>
2019-10-24 13:23:46 -0600

    Put together a basic WPF game

--- BoardGames/TicTacToe/MainWindow.xaml.cs
+++ BoardGames/TicTacToe/MainWindow.xaml.cs
@@ -10,7 +10,6 @@ namespace TicTacToe
     /// </summary>
     public partial class MainWindow : Window
     {
-        bool turnX = true;
         BoardGames.TicTacToe.TicTacToe game = new BoardGames.TicTacToe.TicTacToe();
 
         public MainWindow()
@@ -20,20 +19,25 @@ namespace TicTacToe
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            switch ((sender as Button).Name)
+            var cell = int.Parse((sender as Button).Name[1].ToString()) - 1;
+            if (game.InProgress)
             {
-                case "B1": game = (BoardGames.TicTacToe.TicTacToe)game.Play(0); break;
-                case "B2": game = (BoardGames.TicTacToe.TicTacToe)game.Play(1); break;
-                case "B3": game = (BoardGames.TicTacToe.TicTacToe)game.Play(2); break;
-                case "B4": game = (BoardGames.TicTacToe.TicTacToe)game.Play(3); break;
-                case "B5": game = (BoardGames.TicTacToe.TicTacToe)game.Play(4); break;
-                case "B6": game = (BoardGames.TicTacToe.TicTacToe)game.Play(5); break;
-                case "B7": game = (BoardGames.TicTacToe.TicTacToe)game.Play(6); break;
-                case "B8": game = (BoardGames.TicTacToe.TicTacToe)game.Play(7); break;
-                case "B9": game = (BoardGames.TicTacToe.TicTacToe)game.Play(8); break;
+                game = game.Play(cell);
+                ((sender as Button).Content as TextBlock).Text = (game[cell] == XO.Empty) ? string.Empty : game[cell].ToString();
             }
 
-            turnX = !turnX;
+            if (game.InProgress)
+            {
+                Status.Text = game.Turn.ToString() + "'s turn";
+            } else
+            {
+                Status.Text = game.Winner switch
+                {
+                    XO.X => "X wins!",
+                    XO.O => "O wins!",
+                    _ => "It's a tie!"
+                };
+            }
         }
     }
 }
