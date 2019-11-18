eaecfd5224feb414ce8195fe5c061e89b51fef00
Benjamin Chambers <benjamindchambers@outlook.com>
2019-11-18 13:31:38 -0700

    Added GameState enum

---
+++ BoardGames/BoardGames/Games/GameState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibBoardGames.Games
+{
+    public enum GameState
+    {
+        Invalid,
+        InProgress,
+        Won,
+        Tie
+    }
+}
